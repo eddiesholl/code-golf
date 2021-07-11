eb5afbc137cc9f572ae9cd6a41be22d605088137
Eddie Sholl <esholl@zendesk.com>
2021-07-11 22:56:25 +1000

    First end to end working

--- src/problems/missing-integer.test.js
+++ src/problems/missing-integer.test.js
@@ -1,11 +1,55 @@
-const { findMissingInteger } = require("./missing-integer");
+const { findMissingInteger, insertNumber } = require("./missing-integer");
 
-describe('basic cases', () => {
-  test.each([
-    {name: 'ordered', seq: [1,2,3,4,6,7,8,9], answer:5 }
-  ])('basic case $name', ({ seq, answer }) => {
-    expect(findMissingInteger(seq)).toBe(answer);
+describe('findMissingNumber', () => {
+  describe('basic cases', () => {
+    test.each([
+      {name: 'ordered', seq: [1,2,3,4,6,7,8,9], answer:5 }
+    ])('basic case $name', ({ seq, answer }) => {
+      expect(findMissingInteger(seq)).toEqual(answer);
+    })
+  });
+  // describe('edge cases');
+  describe.skip('invalid cases', () => {
+    test('multiple gaps', () => {
+      expect(() => findMissingInteger([1,2,5,6,8,9]))
+        .toThrow('Too many ranges found')
+    })
+
+    test('no gap', () => {
+      expect(() => findMissingInteger([1,2,3,4,5,6,7,8,9]))
+        .toThrow('Only one range found')
+    })
+  });
+})
+
+describe('insertNumber', () => {
+  const fiveFive = { start: 5, end: 5 }
+  const fiveSix = { start: 5, end: 6 }
+  const fiveSeven = { start: 5, end: 7 }
+  const sevenSeven = { start: 7, end: 7 }
+  const sevenEight = { start: 7, end: 8 }
+  const eightEight = { start: 8, end: 8 }
+  const eightNine = { start: 8, end: 9 }
+
+  describe('modify start', () => {
+    test.each([
+      {name: 'start', range: {...fiveFive}, subject: 6, result: fiveSix },
+      {name: 'start plus', range: {...fiveSix}, subject: 7, result: fiveSeven },
+      {name: 'between ranges', range: {...fiveFive, nextRange: eightEight }, subject: 6, result: {...fiveSix, nextRange: eightEight} },
+      {name: 'join ranges', range: {...fiveFive, nextRange: {...sevenSeven} }, subject: 6, result: {...fiveSeven} },
+
+    ])('insertNumber $name', ({ range, subject, result }) => {
+      expect(insertNumber(range, subject)).toEqual(result)
+    })
   })
-});
-// describe('edge cases');
-// describe('invalid cases');
+
+  describe('modify next', () => {
+    test.each([
+      {name: 'start', range: {...fiveFive, nextRange: {...eightEight }}, subject: 7, result: {...fiveFive, nextRange: sevenEight} },
+      {name: 'end of next', range: {...fiveFive, nextRange: {...eightEight }}, subject: 9, result: {...fiveFive, nextRange: eightNine} }
+    ])('insertNumber $name', ({ range, subject, result }) => {
+      expect(insertNumber(range, subject)).toEqual(result)
+    })
+  })
+
+})
--- src/problems/missing-integer.ts
+++ src/problems/missing-integer.ts
@@ -1,5 +1,84 @@
+type Range = {
+  prevRange?: Range;
+  start: number;
+  end: number;
+  nextRange?: Range;
+};
 
+const findGapIn = (startRange: Range): number => {
+  console.dir(startRange)
+  if (startRange.nextRange === undefined) {
+    throw new Error('Only one range found')
+  } else {
+    const next = startRange.nextRange;
+
+    if (next.start > startRange.end + 2) {
+      throw new Error('Gap is too big')
+    }
+
+    if (next.nextRange !== undefined) {
+      throw new Error('Too many ranges found')
+    }
+
+    return startRange.end + 1;
+  }
+
+  return 0;
+}
+
+export const insertNumber = (startRange: Range, subject: number): Range => {
+  let currentRange: Range | undefined = startRange
+  let nextRange = currentRange.nextRange;
+  let foundMatch = false
+  while (currentRange && !foundMatch) {
+    if (subject === currentRange.end + 1) {
+      if (nextRange !== undefined && nextRange.start === subject + 1) {
+        // join the 2 ranges
+        console.log('join')
+        currentRange.end = nextRange.end
+        currentRange.nextRange = nextRange.nextRange
+        foundMatch = true;
+      } else {        
+        // extend the current range
+        console.log('extend 1')
+        currentRange.end = currentRange.end + 1
+        foundMatch = true;
+      }
+    } else if (subject === currentRange.start - 1) {
+      // extend the current range
+      console.log('extend 2')
+      currentRange.start = currentRange.start - 1
+      foundMatch = true;
+    } else if (currentRange.nextRange === undefined) {
+      // create a new distant range
+      console.log('new')
+      currentRange.nextRange = {
+        start: subject,
+        end: subject
+      }
+      foundMatch = true
+    }
+    currentRange = currentRange.nextRange
+  }
+
+  console.dir(startRange)
+  return startRange;
+}
+
+const buildRanges = (sequence: number[]): Range => {
+  const firstRange = {
+    start: 0,
+    end: 0
+  }
+
+  for (let i = 0; i < sequence.length; i++) {
+    insertNumber(firstRange, sequence[i])  
+  }
+
+  return firstRange;
+}
 
 export const findMissingInteger = (sequence: number[]): number => {
-  return 0;
+  const ranges = buildRanges(sequence);
+  return findGapIn(ranges);
 }
